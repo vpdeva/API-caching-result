82d810450e3f07497444854889328c6306888bb6
Vishnu Prakash Devarajan <vishnuprakhash@gmail.com>
2016-11-08 15:56:13 +1300

    Update config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@
 $GLOBALS['yp_caching_storage'] = 'mysql'; //sqlite, mysql, file
 
 //mysql database access
-$GLOBALS['db_host'] = 'sql.school.kiwi';
-$GLOBALS['db_name'] = 'messages';
-$GLOBALS['db_user'] = 'messages';
-$GLOBALS['db_password'] = 'an2illA';
-$GLOBALS['db_table_name'] = 'hooks';
+$GLOBALS['db_host'] = 'hostname';
+$GLOBALS['db_name'] = 'db';
+$GLOBALS['db_user'] = 'username';
+$GLOBALS['db_password'] = 'password';
+$GLOBALS['db_table_name'] = 'table_name';
 
 ?>
